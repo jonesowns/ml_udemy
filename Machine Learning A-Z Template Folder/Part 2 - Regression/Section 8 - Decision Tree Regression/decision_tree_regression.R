4907684d13c011c4a786f04d9e7f88ddfc7b67c5
jonesowns <yamornothing@gmail.com>
2019-06-07 00:28:39 +0530

    svr + decision tree

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.R
@@ -1,4 +1,4 @@
-# SVR
+# Decision Tree Regression
 
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
@@ -16,18 +16,17 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting SVR to the dataset
-install.packages('e1071')
-library(e1071)
-regressor = svm(formula = Salary ~ .,
-                data = dataset,
-                type = 'eps-regression',
-                kernel = 'radial')
+# Fitting Decision Tree Regression to the dataset
 
-# Predicting a new result
+library(rpart)
+regressor = rpart(formula = Salary ~ .,
+                  data = dataset,
+                  control = rpart.control(minsplit = 1))
+
+# Predicting a new result with Decision Tree Regression
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the SVR results
+
 # install.packages('ggplot2')
 library(ggplot2)
 ggplot() +
@@ -38,15 +37,23 @@ ggplot() +
   ggtitle('Truth or Bluff (SVR)') +
   xlab('Level') +
   ylab('Salary')
-# Visualising the SVR results (for higher resolution and smoother curve)
+
+
+
+
+# Visualising the Decision Tree Regression results (higher resolution)
 # install.packages('ggplot2')
 library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.00011)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +
   geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (SVR)') +
+  ggtitle('Truth or Bluff (Decision Tree Regression)') +
   xlab('Level') +
   ylab('Salary')
+
+# Plotting the tree
+plot(regressor)
+text(regressor)
